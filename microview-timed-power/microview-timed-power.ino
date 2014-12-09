e1e5fe4177db50c892b6bed7b3b213836d59d711
Harry Culpan <harry@tesseract.org>
2014-12-09 07:02:41 -0500

    Added long button press to stop countdown; fixed cap of 60 min

--- microview-timed-power/microview-timed-power.ino
+++ microview-timed-power/microview-timed-power.ino
@@ -3,10 +3,14 @@
 int buttonPin = A0;     	// push button pin
 int buttonState = 0;		// variable to store the pushbutton status
 
+int longButtonPressInSeconds = 2;
+
 signed long countdownSeconds = 0;
 unsigned long countdownStart = 0;
+unsigned long buttonPressStart = 0;
 int buttonPressed = true;
-char outputBuffer[10];
+int longButtonPressed = false;
+char outputBuffer[50];
 
 void setup() {
         uView.begin();			// start MicroView
@@ -15,6 +19,7 @@ void setup() {
         pinMode(buttonPin, INPUT);  	// initialize the pushbutton pin as an input
 	digitalWrite(buttonPin, HIGH); 	// set Internal pull-up
         countdownExpired();
+        Serial.begin(9600);
 }
 
 void countdownExpired() {
@@ -22,6 +27,12 @@ void countdownExpired() {
   countdownStart = 0;
   uView.clear(PAGE);
   uView.display();
+  
+  // Put Power On here
+}
+
+void countdownStarted() {
+  // Put power off here
 }
 
 void loop() {
@@ -32,18 +43,28 @@ void loop() {
           // Pressed for the first time
           if (countdownSeconds == 0) {
             countdownStart = millis()/1000;
+            countdownStarted();
           }
           countdownSeconds += 15 * 60;
           // One-hour max
           if (countdownSeconds > 3600) {
+            countdownStart = millis()/1000;
             countdownSeconds = 3600;
           }
           buttonPressed = true;
         } else if (buttonState == LOW && buttonPressed) { // depressed button
+          buttonPressStart = 0;
           buttonPressed = false;
+        } else if (buttonState == LOW && buttonPressStart == 0) {
+          buttonPressStart = millis()/1000;
+        } else if (buttonState == LOW) {
+          signed long buttonLength = longButtonPressInSeconds - ((millis()/1000) - buttonPressStart);
+          longButtonPressed = (buttonLength < 0);
         }
-        
-        if (countdownSeconds != 0) {
+
+        if (longButtonPressed) {
+          countdownExpired();
+        } else if (countdownSeconds != 0) {
           signed long remaining = countdownSeconds - ((millis()/1000) - countdownStart);
           if (remaining < 1) {
             countdownExpired();
